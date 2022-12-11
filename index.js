69e885a859d250638f6d6a970fcbcbea146e9baa
sha1good <bankoleyakub11@yahoo.com>
2022-12-11 20:14:48 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { videos, user } = require("./fakeData");
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/api/user/1", (req, res) => {
   setTimeout(() => {
